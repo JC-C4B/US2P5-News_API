a60e0760bd0b687d17fa5df978232d7c18a2174d
JC-C4B <136736016+JC-C4B@users.noreply.github.com>
2025-03-20 13:29:44 -0400

    Send email with news titles and descriptions

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import requests
+import smtplib, ssl
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "jcxc4b@gmail.com"
+    password = "GMAILAPPPASSWORD"
+
+    receiver = "jcxc4b@gmail.com"
+    my_context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=my_context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
 APIKey = "833159d37484469fa47c4f3650cce8fd"
 
@@ -11,6 +27,11 @@ request = requests.get(url)
 content = request.json()
 
 #  Accessing the article titles and descriptions
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+body = body.encode("utf-8")
+# Calling our send email function; leaving commented for now
+# send_email(message=body)
